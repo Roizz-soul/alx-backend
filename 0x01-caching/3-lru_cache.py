2b638186914b98d57f14df9918ab512dfd3d200a
Oluremi Abraham <oluremiabraham9@gmail.com>
2024-09-04 15:38:09 +0000

    Task3 check

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -15,10 +15,6 @@ class LRUCache(BaseCaching):
         if key is not None and item is not None:
             self.cache_data[key] = item
             to_pop = ''
-            for i in self.access.keys():
-                self.access[i] -= 1
-
-            self.access[key] = 0
 
             if len(self.cache_data) > BaseCaching.MAX_ITEMS:
                 to_pop = min(self.access, key=self.access.get)
@@ -26,10 +22,16 @@ class LRUCache(BaseCaching):
                 del self.cache_data[to_pop]
                 del self.access[to_pop]
 
+            for i in self.access.keys():
+                self.access[i] -= 1
+            self.access[key] = 0
+
     def get(self, key):
         """ Gets from the dictionary """
         if key is None or self.cache_data.get(key) is None:
             return None
         if self.access != {}:
+            for i in self.access.keys():
+                self.access[i] -= 1
             self.access[key] = 0
         return self.cache_data.get(key)
