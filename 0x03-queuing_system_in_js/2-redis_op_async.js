09718f4c440baec456f5bbe036e3757d0263897b
Oluremi Abraham <oluremiabraham9@gmail.com>
2024-10-09 18:45:57 +0000

    Tasks

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,26 @@
+import { createClient, print } from 'redis';
+import { promisify } from 'util';
+
+const client = createClient();
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err.message}`);
+});
+
+const setNewSchool = (schoolName, value) => {
+  client.set(schoolName, value, print);
+};
+
+const displaySchoolValue = async (schoolName) => {
+  console.log(await promisify(client.GET).bind(client)(schoolName));
+};
+
+async function main() {
+  await displaySchoolValue('Holberton');
+  setNewSchool('HolbertonSanFrancisco', '100');
+  await displaySchoolValue('HolbertonSanFrancisco');
+}
+
+client.on('connect', async () => {
+  console.log('Redis client connected to the server');
+  await main();
+});
